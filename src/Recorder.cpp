2a9f155e73f36bd4d967a57b0f1bfadfb38304b6
winter <me@wint0r.zone>
2025-12-07 20:39:19 +0900

    i forgot to use said variable correctly

--- src/Recorder.cpp
+++ src/Recorder.cpp
@@ -42,7 +42,7 @@ Result<> Recorder::start() {
       return Err("error while initialising microphone audio recorder/ffmpeg: {}", res.err());
     }
 
-    replay_buffer = std::make_shared<ReplayBuffer>(Mod::get()->getSavedValue<int>("settings-length"));
+    replay_buffer = std::make_shared<ReplayBuffer>(Mod::get()->getSavedValue<int>("settings-length") * 1000);
     recorder->init_hook();
     this->first_init = true;
   } else {
